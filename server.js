7ed5726fcddef6ac4f6e64526de00b7ad088c6b7
AvellynL <messinur7@gmail.com>
2025-07-12 21:42:33 +0700

    a

--- server.js
+++ server.js
@@ -25,6 +25,3 @@ app.use('/api/cart', cartRoutes);
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
