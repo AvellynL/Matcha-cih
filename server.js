28f695781fc5d12137d0ad4c91d98e2012be868f
AvellynL <messinur7@gmail.com>
2025-07-12 22:28:38 +0700

    abc

--- server.js
+++ server.js
@@ -16,6 +16,8 @@ app.use(cors());
 app.use(express.json());
 
 app.use(express.static(path.join(__dirname, '')));
+app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use('/js', express.static(path.join(__dirname, 'js')));
 
 app.use('/api/products', productRoutes);
 app.use('/api/orders', orderRoutes);
@@ -25,3 +27,5 @@ app.use('/api/cart', cartRoutes);
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
+
+module.exports = app;
